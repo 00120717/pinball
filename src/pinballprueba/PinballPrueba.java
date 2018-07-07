005ba7f407e84450adc05b32e9d4110479f0707e
Oscar Lovato <Oscar Lovato@LAPTOP-HN0CBI2R>
2018-07-07 16:19:17 -0600

    movi la pelota conan

--- src/pinballprueba/PinballPrueba.java
+++ src/pinballprueba/PinballPrueba.java
@@ -21,7 +21,7 @@ public class PinballPrueba extends JPanel {
 
 	final int xpos = 280;
 	
-	double[] p2d = {280, 200};
+	double[] p2d = {300, 200};
 	double[] v2d = {0, 0};
 	
 	int points = 0;
@@ -100,7 +100,7 @@ public class PinballPrueba extends JPanel {
                     
 		
 		if(p2d[1] > 1000){
-			p2d[0] = 280;
+			p2d[0] = 300;
 			p2d[1] = 200;
 			v2d[0] = 0;
 			v2d[1] = 0;
